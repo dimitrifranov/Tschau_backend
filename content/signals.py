96144c28aa2eedbe1dc1c199e015ed73854b434a
Dimitri Franov <49280846+dimitrifranov@users.noreply.github.com>
2020-08-05 11:09:31 +0200

    autosaved post signal in db

--- content/signals.py
+++ content/signals.py
@@ -12,18 +12,23 @@ import json
 @receiver(post_save, sender=Post)
 def create_post(sender, instance, created, **kwargs):
     if created:
+        message = "nice Post"
         signal_id = instance.creator.profile.signal_id
         app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
         header = {"Content-Type": "application/json; charset=utf-8"}
         payload = {
             "app_id": app_id,
             "include_player_ids": [signal_id],
-            "contents": {"en": "nice Post"},
+            "contents": {"en": message},
         }
         r = requests.post(
             "https://onesignal.com/api/v1/notifications",
             headers=header,
             data=json.dumps(payload),
+        )
+        user = User.objects.get(pk=instance.creator.pk)
+        notif = Notification.objects.create(
+            content=message, user=user,
         )
         # print(r.text)
 
@@ -32,13 +37,14 @@ def create_post(sender, instance, created, **kwargs):
 def create_post_like(sender, instance, created, **kwargs):
     if created:
         # print(instance)
+        message = "hesche like becho vom " + instance.liker.username
         signal_id = instance.post.creator.profile.signal_id
         app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
         header = {"Content-Type": "application/json; charset=utf-8"}
         payload = {
             "app_id": app_id,
             "include_player_ids": [signal_id],
-            "contents": {"en": "hesche like becho vom " + instance.liker.username},
+            "contents": {"en": message},
         }
         r = requests.post(
             "https://onesignal.com/api/v1/notifications",
@@ -47,7 +53,7 @@ def create_post_like(sender, instance, created, **kwargs):
         )
         user = User.objects.get(pk=instance.post.creator.pk)
         notif = Notification.objects.create(
-            content="hesche like becho vom " + instance.liker.username, user=user,
+            content=message, user=user,
         )
         # print(r.text)
 
