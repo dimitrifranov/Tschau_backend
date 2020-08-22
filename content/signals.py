cf630422390deab3363385e8ddee6f8f0e5f44c5
Dimitri Franov <dfranov01@gmail.com>
2020-08-22 17:36:13 +0200

    follower and comment notification added

--- content/signals.py
+++ content/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from content.models import Post, PostLike, Notification
+from content.models import Post, PostLike, Notification, Comment
 
 import os
 import requests
@@ -27,9 +27,31 @@ def create_post(sender, instance, created, **kwargs):
             data=json.dumps(payload),
         )
         user = User.objects.get(pk=instance.creator.pk)
-        notif = Notification.objects.create(
-            content=message, user=user,
+        notif = Notification.objects.create(content=message, user=user,)
+        # print(r.text)
+
+@receiver(post_save, sender=Post)
+def create_post(sender, instance, created, **kwargs):
+    if created:
+        message = instance.creator.username + 'hat einen neuen Beitrag geteilt.'
+        for(follower of instance.creator.follower){
+            signal_id = follower.profile.signal_id
+        app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
+        header = {"Content-Type": "application/json; charset=utf-8"}
+        payload = {
+            "app_id": app_id,
+            "include_player_ids": [signal_id],
+            "contents": {"en": message},
+        }
+        r = requests.post(
+            "https://onesignal.com/api/v1/notifications",
+            headers=header,
+            data=json.dumps(payload),
         )
+        user = User.objects.get(pk=follower.pk)
+        notif = Notification.objects.create(content=message, user=user,)
+        }
+        
         # print(r.text)
 
 
@@ -52,16 +74,29 @@ def create_post_like(sender, instance, created, **kwargs):
             data=json.dumps(payload),
         )
         user = User.objects.get(pk=instance.post.creator.pk)
-        notif = Notification.objects.create(
-            content=message, user=user,
-        )
+        notif = Notification.objects.create(content=message, user=user,)
         # print(r.text)
 
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     # user = instance
-#     # if created:
-#     #     profile = UserProfile(user=user)
-#     #     profile.save()
-#     instance.profile.save()
+@receiver(post_save, sender=Comment)
+def create_comment(sender, instance, created, **kwargs):
+    if created:
+        # print(instance)
+        message = instance.creator.username + "hat deinen Beitrag kommentiert."
+        signal_id = instance.post.creator.profile.signal_id
+        app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
+        header = {"Content-Type": "application/json; charset=utf-8"}
+        payload = {
+            "app_id": app_id,
+            "include_player_ids": [signal_id],
+            "contents": {"en": message},
+        }
+        r = requests.post(
+            "https://onesignal.com/api/v1/notifications",
+            headers=header,
+            data=json.dumps(payload),
+        )
+        user = User.objects.get(pk=instance.post.creator.pk)
+        notif = Notification.objects.create(content=message, user=user,)
+        # print(r.text)
+
