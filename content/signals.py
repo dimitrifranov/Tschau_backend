6dfb6bfef79d4f16044bb927cca889babe886ddd
Dimitri Franov <dfranov01@gmail.com>
2020-08-22 18:36:10 +0200

    removed nice post(syntax)

--- content/signals.py
+++ content/signals.py
@@ -13,22 +13,25 @@ import json
 def create_post(sender, instance, created, **kwargs):
     if created:
         message = instance.creator.username + "hat einen neuen Beitrag geteilt."
+        follower_ids = []
         for follower in instance.creator.follower:
-            signal_id = follower.profile.signal_id
-            app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
-            header = {"Content-Type": "application/json; charset=utf-8"}
-            payload = {
-                "app_id": app_id,
-                "include_player_ids": [signal_id],
-                "contents": {"en": message},
-            }
-            r = requests.post(
-                "https://onesignal.com/api/v1/notifications",
-                headers=header,
-                data=json.dumps(payload),
-            )
-            user = User.objects.get(pk=follower.pk)
+            follower_ids.append(follower.profile.signal_id)
+             user = User.objects.get(pk=follower.pk)
             notif = Notification.objects.create(content=message, user=user,)
+
+        app_id = "56c16a44-f980-41c2-8a74-b4591cc6ab35"
+        header = {"Content-Type": "application/json; charset=utf-8"}
+        payload = {
+            "app_id": app_id,
+            "include_player_ids": [follower_ids],
+            "contents": {"en": message},
+        }
+        r = requests.post(
+            "https://onesignal.com/api/v1/notifications",
+            headers=header,
+            data=json.dumps(payload),
+        )
+            
 
         # print(r.text)
 
