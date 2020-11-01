d5249f498dba744cc60bfa783725cf41c093725a
Dimitri Franov <dfranov01@gmail.com>
2020-11-01 16:20:43 +0100

    added all models to admin panel

--- content/admin.py
+++ content/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from content.models import Post, Comment, Group
+from content.models import Post, Comment, Group, Membership, Notification, PostLike
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(Notification)
+admin.site.register(PostLike)
